b0bb10f036ee20014b4a904b832fe2f5a8c462bd
Leonardo <leo.cortelim@gmail.com>
2025-10-31 10:39:58 -0300

    Script

---
+++ Assets/Script/Teste.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.Build.Content;
+using UnityEngine;
+
+public class Teste : MonoBehaviour
+{
+    public float x, z;
+    float velocidade;
+    LineRenderer lr;
+    Rigidbody rb;
+
+
+    void Start()
+    {
+        lr = GetComponent<LineRenderer>();
+        rb = GetComponent<Rigidbody>();
+        if (rb != null)
+            Debug.Log("");
+
+    }
+
+    void Update()
+    {
+        lr.SetPosition(0, transform.position);
+        lr.SetPosition(1, new Vector3(transform.position.x - x,
+            transform.position.z + z));
+        if (Input.GetButtonDown("Jump") && lr.enabled)
+        {
+            rb.AddForce(new Vector3(2 * x, 0, 2 * z), ForceMode.Impulse);
+            lr.enabled = false;
+            if (GameManager.gm)
+            {
+
+                GameManager.gm.tacada();
+            }
+
+        }
+        velocidade = rb.velocity.magnitude;
+        if (velocidade < 0.05f)
+        {
+
+            rb.velocity = Vector3.zero;
+            rb.angularVelocity = Vector3.zero;
+            lr.enabled = true;
+        }
+        else
+        {
+            lr.enabled = false;
+        }
+    }
+}
